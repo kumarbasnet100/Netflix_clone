8c9f07db806d36c27e3261ab7aa09cb5494e05de
Kumar Basnet <kumarkingxeetri100@gmail.com>
2025-02-14 01:43:31 +0545

    fixed TitleCards.css

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./TitleCards.css";
+import './TitleCards.css';
 import cards_data from "../../assets/cards/Cards_data";
 import { Link } from "react-router-dom";
 
